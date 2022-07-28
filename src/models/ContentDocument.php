557b595cd9db2964085c51c1f03968390eb32493
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-07-28 11:04:17 -0700

    Add properties, and a factory method for building custom 'ContentDocument' objects.

--- src/models/ContentDocument.php
+++ src/models/ContentDocument.php
@@ -8,10 +8,18 @@ class ContentDocument extends SalesforceFile { // implements ISObject
     public $SObjectName = "ContentVersion";
 
     protected $Id;
-
     private $ContentDocumentId;
-
     private $LinkedEntityId;
+
+    private $id;
+    private $title;
+    private $fileSize;
+    private $extension;
+    private $fileType;
+    private $ownerName;
+    private $ownerId;
+    private $linkedEntityId;
+
 
     public $isLocal = false;
 
@@ -49,6 +57,55 @@ class ContentDocument extends SalesforceFile { // implements ISObject
 
         return $this->ContentDocumentId;
     }
+
+
+
+    public function id() {
+
+        return $this->id;
+    }
+
+    public function title() {
+
+        return $this->title;
+    }
+
+    public function fileSize() {
+
+        return $this->fileSize;
+    }
+
+    public function fileType() {
+
+        return $this->fileType;
+    }
+
+    public function extension() {
+
+        return $this->extension;
+    }
+
+    public function uploadedBy() {
+
+        return empty($this->uploadedBy) ? "OCDLA APP" : $this->uploadedBy;
+    }
+
+    public function ownerId() {
+
+        return $this->ownerId;
+    }
+
+    public function linkedEntityId() {
+
+        return $this->linkedEntityId;
+    }
+
+    public function userIsOwner() {
+
+        return current_user()->getContactId() == $this->ownerId;
+    }
+
+    
     public static function fromFile(File $file){
 
         $sfFile = new ContentDocument();
@@ -65,6 +122,31 @@ class ContentDocument extends SalesforceFile { // implements ISObject
         $sfFile->Id = $ojb["id"];
 
         return $sfFile;
+    }
+
+    public static function fromContentDocumentLinkQueryResult($contentDocumentLinkQueryResults) {
+
+        $documents = [];
+
+        foreach($contentDocumentLinkQueryResults as $result) {
+
+            $data = $result["ContentDocument"];
+
+            $doc = new self();
+            $doc->id = $result["ContentDocumentId"];
+            $doc->title = $data["Title"];
+            $doc->fileSize = calculateFileSize($data["ContentSize"]);
+            $doc->fileType = $data["FileType"];
+            $doc->extension = $data["FileExtension"];
+            $doc->uploadedBy = $result["ownerName"];
+            $doc->ownerId = $result["ownerId"];
+            $doc->linkedEntityId = $result["LinkedEntityId"];
+            
+
+            $documents[] = $doc;
+        }
+
+        return $documents;
     }
 
     public static function fromJson($json){
