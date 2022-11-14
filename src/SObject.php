574b7f72223434d2a553b83b0dec55fd4e98da07
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2022-11-14 21:45:12 +0000

    Resolve conflicts.

--- src/SObject.php
+++ src/SObject.php
@@ -25,6 +25,58 @@ class SObject {
         return $this->sobject[$name];
     }
 
+    public function getId() {
+        return $this->Id;
+    }
+
+
+    public static function fromSObjects($records){
+
+        return array_map(function($r) {
+            $c = new self($r["Id"]);
+            // $c->AreasOfInterest__r = $r["AreasOfInterest__r"]["records"];
+            foreach(array_keys($r) as $key) {
+                $c->{$key} = $r[$key];
+            }
+
+            return $c;
+        }, $records);
+    }
+
+
+    /**
+     * Examples:
+     *  // Get all selected values for this SObject's multipicklist "myPicklist" field.
+     *  ->getValues("myPicklist");
+     * 
+     *  // You can also specify a relationship and field pair.
+     *  ->getValues("AreasOfInterest__r.Interest__c");
+     * 
+     *  // Or specify multiple fields to retrieve in the query.
+     *  -->getValues("AreasOfInterest__r.Interest__c","AreasOfInterest__r.Id");
+     * 
+     * @param query String A string specifying SObject query syntax.  SObject query syntax is a shorthand relationship/object/field identifiers to retrieve scalar or array values from this Salesforce record.
+     */
+    public function getObject($query) {
+
+        $parts = explode(".", $query);
+
+        $field1 = array_shift($parts);
+        
+		$start = $this->{$field1};
+
+        if(count($parts) == 0) {
+            return $start;
+        }
+
+        return array_reduce($parts, function($carry, $current) {
+            return $carry[$current];
+        }, $start);
+    }
+
+
+
+    
     public static function toList($records) {
 
         $list = array();
